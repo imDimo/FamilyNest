8e01f8d56835398e06fe5ea640f07a40d7f44ceb
Nathaniel <nathanielparise@proton.me>
2026-05-02 10:56:24 -0500

    Remove unnecessary import and specify redis as the session storage in application.properties

--- src/main/java/edu/uwgb/se372/familynest/UserNavigationController.java
+++ src/main/java/edu/uwgb/se372/familynest/UserNavigationController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.uwgb.se372.familynest.user.NestUserService;
 import jakarta.servlet.RequestDispatcher;
