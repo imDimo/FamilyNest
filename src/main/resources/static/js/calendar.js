9c3014e8ca0140f6461f3dc08c6b22125d84242d
Nathaniel <nathanielparise@proton.me>
2026-05-04 20:27:43 -0500

    Get event containers from calendar display

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -2,13 +2,28 @@ let stompClient = null;
 
 const mySenderId = "user-" + Math.random().toString(36).substring(2, 9);
 
+let dayContainers = [];
+
 function main() {
+    initEvents();
     let bttn = document.getElementById("announceButton");
     if (bttn) {
         bttn.addEventListener("click", sendAnnouncement);
     }
     
     connect(); 
+
+}
+
+function initEvents() {
+    for (let i = 1;; i++) {
+        let day_container = document.getElementById(`day-${i}`);
+        if (!day_container) {
+            break;
+        }
+
+        dayContainers.add(day_container);
+    }
 }
 
 function sendAnnouncement() {
@@ -33,6 +48,16 @@ function connect() {
     stompClient = new StompJs.Client({
         brokerURL: url,
         onConnect: () => {
+            stompClient.subscribe("/topic/events", (msg) => {
+                const data = JSON.parse(msg.body);
+                console.log("Event received:", data); 
+                // if (data.senderId && data.senderId !== mySenderId) {
+                    // showAnnouncement(data);
+                // }
+                
+                console.log("Event name: " + data.title);
+            });
+
             stompClient.subscribe("/topic/announcements", (msg) => {
                 const data = JSON.parse(msg.body);
                 console.log("Announcement received:", data); 
